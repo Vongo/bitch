381ff98faceac85f02a440f17e4bfcb88165bc90
Vongo <adelmarre@vongo.eu>
2014-08-22 10:49:00 +0200

    improved results

--- cooccurrences.R
+++ cooccurrences.R
@@ -26,6 +26,8 @@ transform <- function(x,s=NA,t=NA) {
 graph <- data.frame(matrix(vector(), 0, 3, dimnames=list(c(), c("Source","Target","Strength"))), stringsAsFactors=F)
 
 data <- read.csv("CLEAN_ANONYMOUS.csv", sep = ",", header = T, stringsAsFactors = F)
+data$Content <- tolower(data$Content)
+
 # Naive implementation
 for (a in ACTORS) {
 	incoming.data <- data[data$Target==a,]
@@ -34,11 +36,22 @@ for (a in ACTORS) {
 	outgoing.vocabulary <- unlist(lapply(outgoing.data$Content,function(x) unlist(strsplit(x," "))))
 	incoming.strong.vocabulary <- incoming.vocabulary[nchar(incoming.vocabulary)>3]
 	outgoing.strong.vocabulary <- outgoing.vocabulary[nchar(outgoing.vocabulary)>3]
-	graph <- rbind(graph,transform(incoming.strong.vocabulary,t=a))
-	graph <- rbind(graph,transform(outgoing.strong.vocabulary,s=a))
+	a.inc.voc <- transform(incoming.strong.vocabulary,t=a)
+	a.out.voc <- transform(outgoing.strong.vocabulary,s=a)
+	tryCatch({
+		graph <- rbind(graph,a.out.voc)
+		graph <- rbind(graph,a.inc.voc)
+	}, warning = function(w) {
+		print(w)
+	}, error = function(e) {
+	    print(paste("Failed for user",a,"with error",e))
+	    print(dim(a.inc.voc))
+	    print(dim(a.out.voc))
+	}, finally = function(e){
+	})
 }
 
-graph <- graph[complete.cases(graph) & graph$Strength > 5,]
+graph <- graph[complete.cases(graph) & graph$Strength >= 4,]
 
 write.table(graph,"vocable.csv",
 	append = FALSE, quote = TRUE, sep = ",",
