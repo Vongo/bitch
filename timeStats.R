3e5e08637adf3810b876011f4230100f2c2e0d78
Vongo <adelmarre@vongo.eu>
2014-08-25 14:33:28 +0200

    classy time stats computation

--- timeStats.R
+++ timeStats.R
@@ -14,31 +14,54 @@ vectorize.factor <- function(v) {
 	y
 }
 
-
-avgLength.by.hourInDay <- function(data, lengthCol,...) {
-	vectorize.factor(factor(HOURS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$hour],levels=HOURS))	
+avgLength.by.hourInDay <- function(data, timeCol,...) {
+	zzz <- HOURS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$hour+1]
+	datab <- cbind(data,zzz)
+	avg <- vector()
+	for (h in HOURS)
+		avg <- c(avg,mean(datab[datab$zzz==h,"Length"]))
+	# names(avg) <- sapply(HOURS,function(x) paste(x,"h",sep=""))
+	names(avg) <- HOURS
+	avg[is.nan(avg)] <- 0
+	avg
 }
 
-avgLength.by.dayInMonth <- function(data, lengthCol,...) {
-
+avgLength.by.dayInMonth <- function(data, timeCol,...) {
+	zzz <- MDAYS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$mday]
+	datab <- cbind(data,zzz)
+	avg <- vector()
+	for (d in MDAYS)
+		avg <- c(avg,mean(datab[datab$zzz==d,"Length"]))
+	names(avg) <- MDAYS
+	avg[is.nan(avg)] <- 0
+	avg
 }
 
-avgLength.by.dayInWeek <- function(data, lengthCol,...) {
-
+avgLength.by.dayInWeek <- function(data, timeCol,...) {
+	zzz <- WDAYS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$wday+1]
+	datab <- cbind(data,zzz)
+	avg <- vector()
+	for (d in WDAYS)
+		avg <- c(avg,mean(datab[datab$zzz==d,"Length"]))
+	names(avg) <- WDAYS
+	avg[is.nan(avg)] <- 0
+	avg
 }
 
-avgLength.by.monthInYear <- function(data, lengthCol,...) {
-
+avgLength.by.monthInYear <- function(data, timeCol,...) {
+	zzz <- MONTHS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$mon+1]
+	datab <- cbind(data,zzz)
+	avg <- vector()
+	for (m in OMONTHS)
+		avg <- c(avg,mean(datab[datab$zzz==m,"Length"]))
+	names(avg) <- OMONTHS
+	avg[is.nan(avg)] <- 0
+	avg
 }
-
-avgLength.by.time <- function(data, lengthCol,...) {
-
-}
-
 
 # Assumes that schema(data) contains (timeCol) as a (character) column formatted like ('%d/%m/%Y %H:%M')
 aggreg.by.hourInDay <- function(data, timeCol,...) {
-	vectorize.factor(factor(HOURS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$hour],levels=HOURS))
+	vectorize.factor(factor(HOURS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$hour+1],levels=HOURS))
 }
 
 # Assumes that schema(data) contains (timeCol) as a (character) column formatted like ('%d/%m/%Y %H:%M')
@@ -54,11 +77,6 @@ aggreg.by.dayInWeek <- function(data, timeCol,...) {
 # Assumes that schema(data) contains (timeCol) as a (character) column formatted like ('%d/%m/%Y %H:%M')
 aggreg.by.monthInYear <- function(data, timeCol,...) {
 	vectorize.factor(factor(MONTHS[as.POSIXlt(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))$mon + 1],levels=OMONTHS))
-}
-
-# Assumes that schema(data) contains (timeCol) as a (character) column formatted like ('%d/%m/%Y %H:%M')
-aggreg.by.time <- function(data, timeCol,...) {
-	as.POSIXct(strptime(data[,timeCol],format='%d/%m/%Y %H:%M'))
 }
 
 # Frequency of mail transactions by hour in day (range 1-24)
@@ -86,14 +104,6 @@ img.by.monthInYear <- function(data, ttl, yLeg = "Count") {
 	qplot(x=factor(OMONTHS,levels=OMONTHS),y=data[1:12],stat="identity",geom="histogram") + xlab("Month") + labs(title = ttl) + ylab(yLeg)
 }
 
-# Absolute frequency of mail transactions for all data (auto scale)
-# Assumes that data is a vector already aggregated
-# Avoid this function, still buggy
-img.by.time <- function(data, ttl, yLeg = "Count") {
-	qplot(data) + xlab("Time slot") + labs(title = ttl) +  + ylab(yLeg)
-		scale_x_datetime(labels = date_format("%m"),breaks = date_breaks("6 month"),minor_breaks = date_breaks("1 month"))
-}
-
 # Calculates the ratio x/y
 # If y is close or equal to 0, the ratio won't diverge to infinite value but be equal to 10.
 ratio <- function(x,y) {
@@ -102,43 +112,47 @@ ratio <- function(x,y) {
 
 # the variables "by.x" are pointers to the functions "img.by.x" and "aggreg.by.x"
 # these couples of functions can be accessed as list elements, e.g. imgByX <- by.x[[1]]
-by.hourInDay <- c(img.by.hourInDay,aggreg.by.hourInDay)
-by.dayInMonth <- c(img.by.dayInMonth,aggreg.by.dayInMonth)
-by.dayInWeek <- c(img.by.dayInWeek,aggreg.by.dayInWeek)
-by.monthInYear <- c(img.by.monthInYear,aggreg.by.monthInYear)
-by.time <- c(img.by.time,aggreg.by.time)
-
+by.hourInDay <- c(img.by.hourInDay,aggreg.by.hourInDay,avgLength.by.hourInDay)
+by.dayInMonth <- c(img.by.dayInMonth,aggreg.by.dayInMonth,avgLength.by.dayInMonth)
+by.dayInWeek <- c(img.by.dayInWeek,aggreg.by.dayInWeek,avgLength.by.dayInWeek)
+by.monthInYear <- c(img.by.monthInYear,aggreg.by.monthInYear,avgLength.by.monthInYear)
 
 # Many things to say here.
 stats.by.actor.detail <- function() {
 
 	## WARNING : this function is context-dependant
 	images <- function(fun,path,...) {
+		dir.create("./pic/",showWarnings=F)
+		dir.create("./pic/count/",showWarnings=F)
+		dir.create("./pic/length/",showWarnings=F)
+		dir.create(paste("./pic/count/",path,sep=""),showWarnings=F)
+		dir.create(paste("./pic/length/",path,sep=""),showWarnings=F)
+
 		# Global stats
 		img <- fun[[1]]
 		aggreg <- fun[[2]]
-		dRcv <- aggreg(messages[messages$Target==BITCH,],"DateTime",...)
-		dSnt <- aggreg(messages[messages$Source==BITCH,],"DateTime",...)
-		pRcv <- img(dRcv,"Received",...)
-		pSnt <- img(dSnt,"Sent",...)
-		dRpt <- ratio(dRcv,dSnt)
-		pRpt <- img(dRpt,"Received/Sent")
+		avgLength <- fun[[3]]
 
-		dir.create(paste("./pic/",path,sep=""),showWarnings=F)
-		png(file=paste("./pic/",path,"/Global.png",sep=""),width=756,height=756)
-		multiplot(pRcv,pSnt,pRpt,cols=1)
-		dev.off()
-
-		# Actors
-		for (actor in ACTORS) {
-			dRcv <- aggreg(messages[messages$Source==actor,],"DateTime",...)
-			dSnt <- aggreg(messages[messages$Target==actor,],"DateTime",...)
+		for (actor in c(ACTORS,BITCH)) {
+			dRcv <- aggreg(messages[messages$Target==actor,],"DateTime",...)
+			dSnt <- aggreg(messages[messages$Source==actor,],"DateTime",...)
 			pRcv <- img(dRcv,"Received",...)
 			pSnt <- img(dSnt,"Sent",...)
 			dRpt <- ratio(dRcv,dSnt)
 			pRpt <- img(dRpt,"Received/Sent")
 
-			png(file=paste("./pic/",path,"/",actor,".png",sep=""),width=756,height=756)
+			png(file=paste("./pic/count/",path,"/",actor,".png",sep=""),width=756,height=756)
+			multiplot(pRcv,pSnt,pRpt,cols=1)
+			dev.off()
+
+			dRcv <- avgLength(lengths[lengths$Target==actor,],"DateTime",...)
+			dSnt <- avgLength(lengths[lengths$Source==actor,],"DateTime",...)
+			pRcv <- img(dRcv,"Received",yLeg="avg Length",...)
+			pSnt <- img(dSnt,"Sent",yLeg="avg Length",...)
+			dRpt <- ratio(dRcv,dSnt)
+			pRpt <- img(dRpt,"Received/Sent",yLeg="avg Length")
+
+			png(file=paste("./pic/length/",path,"/",actor,".png",sep=""),width=756,height=756)
 			multiplot(pRcv,pSnt,pRpt,cols=1)
 			dev.off()
 		}
@@ -160,8 +174,5 @@ stats.by.actor.detail <- function() {
 	images(by.dayInMonth,"DayInMonth")
 	if(VERBOSE) cat(paste("Charts : month...\n"))
 	images(by.monthInYear,"MonthInYear")
-	# Except this one : lot of exceptions and (yet) unhandled cases
-	cat(paste("Charts : global...\n"))
-	images(by.time,"General")
 	if(VERBOSE) cat(paste("It's all done.\n"))
 }
